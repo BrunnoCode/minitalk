ef531bd9094462245aadd3bd4ba832331fffbbe9
Brunno Warley Botelho Rocha <bbotelho@cbr9s5.42barcelona.com>
2024-03-13 19:10:23 +0100

    atomatic save

--- ft_cliente.c
+++ ft_cliente.c
@@ -6,62 +6,8 @@
 /*   By: bbotelho <bbotelho@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 17:21:44 by bbotelho          #+#    #+#             */
-/*   Updated: 2024/03/13 17:21:48 by bbotelho         ###   ########.fr       */
+/*   Updated: 2024/03/13 19:09:44 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static void	action(int sig)
-{
-	static int	received = 0;
 
-	if (sig == SIGUSR1)
-		++received;
-	else
-	{
-		ft_putnbr_fd(received, 1);
-		ft_putchar_fd('\n', 1);
-		exit(0);
-	}
-}
-
-static void	mt_kill(int pid, char *str)
-{
-	int		i;
-	char	c;
-
-	while (*str)
-	{
-		i = 8;
-		c = *str++;
-		while (i--)
-		{
-			if (c >> i & 1)
-				kill(pid, SIGUSR2);
-			else
-				kill(pid, SIGUSR1);
-			usleep(100);
-		}
-	}
-	i = 8;
-	while (i--)
-	{
-		kill(pid, SIGUSR1);
-		usleep(100);
-	}
-}
-
-int	main(int argc, char **argv)
-{
-	if (argc != 3 || !ft_strlen(argv[2]))
-		return (1);
-	ft_putstr_fd("Sent    : ", 1);
-	ft_putnbr_fd(ft_strlen(argv[2]), 1);
-	ft_putchar_fd('\n', 1);
-	ft_putstr_fd("Received: ", 1);
-	signal(SIGUSR1, action);
-	signal(SIGUSR2, action);
-	mt_kill(ft_atoi(argv[1]), argv[2]);
-	while (1)
-		pause();
-	return (0);
-}
--- ft_server.c
+++ ft_server.c
@@ -6,52 +6,8 @@
 /*   By: bbotelho <bbotelho@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 17:21:38 by bbotelho          #+#    #+#             */
-/*   Updated: 2024/03/13 17:21:39 by bbotelho         ###   ########.fr       */
+/*   Updated: 2024/03/13 19:09:52 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
-
-
-static void	action(int sig, siginfo_t *info, void *context)
-{
-	static int				i = 0;
-	static pid_t			client_pid = 0;
-	static unsigned char	c = 0;
-
-	(void)context;
-	if (!client_pid)
-		client_pid = info->si_pid;
-	c |= (sig == SIGUSR2);
-	if (++i == 8)
-	{
-		i = 0;
-		if (!c)
-		{
-			kill(client_pid, SIGUSR2);
-			client_pid = 0;
-			return ;
-		}
-		ft_putchar_fd(c, 1);
-		c = 0;
-		kill(client_pid, SIGUSR1);
-	}
-	else
-		c <<= 1;
-}
-
-int	main(void)
-{
-	struct sigaction	s_sigaction;
-
-	ft_putstr_fd("Server PID: ", 1);
-	ft_putnbr_fd(getpid(), 1);
-	ft_putchar_fd('\n', 1);
-	s_sigaction.sa_sigaction = action;
-	s_sigaction.sa_flags = SA_SIGINFO;
-	sigaction(SIGUSR1, &s_sigaction, 0);
-	sigaction(SIGUSR2, &s_sigaction, 0);
-	while (1)
-		pause();
-	return (0);
-}
