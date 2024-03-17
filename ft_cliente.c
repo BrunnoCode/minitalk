cb68f4f331f2a557070625707ef431c002a513e8
Brunno Warley Botelho Rocha <bbotelho@car15s5.42barcelona.com>
2024-03-17 11:10:03 +0100

    save error

--- ft_cliente.c
+++ ft_cliente.c
@@ -6,13 +6,19 @@
 /*   By: bbotelho <bbotelho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 17:21:44 by bbotelho          #+#    #+#             */
-/*   Updated: 2024/03/14 13:12:55 by bbotelho         ###   ########.fr       */
+/*   Updated: 2024/03/14 18:47:17 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_minitalk.h"
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-  
+	if (ac == 2)
+	{
+		unsigned int servpid;
+		servpid = ft_atoi(av[1]);
+    
+	}
+	return (1);
 }
--- ft_server.c
+++ ft_server.c
@@ -6,38 +6,43 @@
 /*   By: bbotelho <bbotelho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 17:21:38 by bbotelho          #+#    #+#             */
-/*   Updated: 2024/03/14 13:11:02 by bbotelho         ###   ########.fr       */
+/*   Updated: 2024/03/14 18:48:58 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_minitalk.h"
 
-void  handler_sig(int sig)
+void	handler_sig(int sig)
 {
-  ft_printf("Hola cheguei!. sinal = %d\n", sig);
+	if (sig == SIGUSR1)
+		ft_printf("SIGUSR1 RECEBIDO\n");
+	if (sig == SIGUSR2)
+		ft_printf("SIGUSR2\n");
+	ft_printf("RECEBIDO sinal: %d\n", sig);
 }
 
-int main()
+int	main(void)
 {
-  pid_t spid;
+	struct sigaction sa;
+	sa.__sigaction_u.__sa_handler = handler_sig;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
   
-  spid = getpid();
-  if (spid == -1)
-  {
-      ft_printf("ERROR: fail to get pid process.\n"); 
-      exit(1); 
-  }
-  ft_printf("Server ready: ok\n");
-  sleep(1);
-  ft_printf("PID Server Information: %i\n", spid);
+	pid_t spid;
+	spid = getpid();
   
-  if (signal(SIGUSR1, handler_sig) == SIG_ERR)
-  {
-      ft_printf("Signal Error!\n");
-      exit(1);
-  }
-  while (1)
-  {
-    signal(SIGUSR1, handler_sig);
-  }
+	if (spid == -1)
+	{
+		ft_printf("ERROR: fail to get pid process.\n");
+		exit(1);
+	}
+	ft_printf("Server ready: ok\n");
+	sleep(1);
+	ft_printf("PID Server Information: %i\n", spid);
+	if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1)
+	{
+		ft_printf("Signal Error!\n");
+		exit(1);
+	}
+	while (1);
 }
