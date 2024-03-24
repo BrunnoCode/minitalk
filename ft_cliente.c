b9f2de4bd27bbeeb6e958cbc5ea6028d268796ce
bbotelho <brunnowr.bw@gmail.com>
2024-03-24 12:10:28 +0100

    atomatic save

--- ft_cliente.c
+++ ft_cliente.c
@@ -6,31 +6,43 @@
 /*   By: bbotelho <bbotelho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 17:21:44 by bbotelho          #+#    #+#             */
-/*   Updated: 2024/03/18 14:17:43 by bbotelho         ###   ########.fr       */
+/*   Updated: 2024/03/24 12:01:45 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_minitalk.h"
 
-int	main(int ac, char **av)
+static void	ft_kill(int pid, char *str)
 {
-	if (ac == 3)
+	int				i;
+	int				bits;
+	unsigned char	c;
+
+	i = 0;
+	while (str[i])
 	{
-		pid_t servpid;
-		int i;
-		unsigned char c;
-		servpid = ft_atoi(av[1]);
-		c = av[2][0];
-		i = 7;
-		while (i >= 0)
+		bits = 8;
+		c = str[i];
+		while (bits--)
 		{
-			if (((c >> i) & 1) == 1)
-				kill(servpid, SIGUSR1);
+			if (c >> bits & 1)
+				kill(pid, SIGUSR1);
 			else
-				kill(servpid, SIGUSR2);
-			i--;
+				kill(pid, SIGUSR2);
 			usleep(100);
 		}
+		i++;
 	}
+}
+
+int	main(int ac, char **av)
+{
+	if (ac != 3)
+	{
+		ft_printf("\033[91mError: Try to use \033[0m");
+		ft_printf("\033[33m./client <PID SERVER> <STRING>.\033[0m\n");
+		exit(1);
+	}
+	ft_kill(ft_atoi(av[1]), av[2]);
 	return (0);
 }
--- ft_server.c
+++ ft_server.c
@@ -6,53 +6,34 @@
 /*   By: bbotelho <bbotelho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 17:21:38 by bbotelho          #+#    #+#             */
-/*   Updated: 2024/03/24 10:51:37 by bbotelho         ###   ########.fr       */
+/*   Updated: 2024/03/24 12:10:18 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_minitalk.h"
 
- void	handler_sig(int sig)
+void	handler_sig(int sig)
 {
-// 	static	char byte = 0;
-// 	static int bit = 8;
 	
-// 	if (sig == SIGUSR1)
-// 	{			
-	
-	if (sig == SIGINT)
-	{
-		ft_printf("\033[91mFinishing Server...\033[0m\n");
-		sleep(1);
-		ft_printf("\033[32mServer Finished success!\033[0m\n");
-		exit(1);
-	}
 }
-	
+
 int	main(void)
 {
-	struct sigaction sa;
-	sa.sa_handler = handler_sig;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0;
-  
-	pid_t spid;
+	pid_t	spid;
+
 	spid = getpid();
-  
 	if (spid == -1)
 	{
-		ft_printf("ERROR: fail to get pid process.\n");
+		ft_printf("\033[91mERROR: fail to get pid process.\033[0m\n");
 		exit(1);
 	}
 	ft_printf("\033[32mServer ready: ok\033[0m\n");
- 	sleep(1);
+	sleep(1);
 	ft_printf("\033[33mServer Information PID:\033[0m %d\n", spid);
-	if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1)
-	{
-		ft_printf("\033[91mError: Signal.\033[0m\n");
-		exit(1);
-	}
-	sigaction(SIGINT, &sa, NULL);
 	while (1)
+	{
+		signal(SIGUSR1, handler_sig);
+		signal(SIGUSR2, handler_sig);
 		pause();
+	}
 }
